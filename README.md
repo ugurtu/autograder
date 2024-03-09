499321cc3bc973d3d2b303aff63e6fbd8d6f6662
Mark Joseph Starzynski <josphstar@protonmail.com>
2024-03-09 06:21:28 +0100

    Update README and clean up downloader

--- README.md
+++ README.md
@@ -1,7 +1,45 @@
 # Auto-grader
-This is an automated grader that evaluates students' grades.
 
-### Structure
+This is an automated grader that evaluates students' grades. As of now it is structured into 3 parts:
+
+- Auto-downloader: Downloads specified submissions from all students who submitted as ZIP file and extracts it into the 'autograder' directory. Script: `adam.py`
+- Auto-grader: Takes downloaded hand-ins (Jupyter Notebooks of students) and performs otter-grader checks; summarizes results into a spreadsheet. Scripts: `main.py` and `insurance_analysis.py`
+- [WORK-IN-PROGRESS] Auto-publisher: Publishes auto-graded results onto ADAM as feedback for the students.
+
+#### Contents
+- 1. Setup and Virtual Environment
+- 2. Structure
+  - 2.1 Structure of Auto-downloader
+  - 2.2 Structure of Auto-grader
+  - 2.3 Structure of Auto-publisher [WIP]
+
+## 1. Setup and Virtual Environment
+- Setup:
+  - Clone repo, `cd autograder`
+
+- Create and setup virtual environment:
+  - Create virtual environment `python -m venv venv`
+  - `source venv/bin/activate`
+  - `pip3 install -r requirements.txt`
+  - to quit virtual environment: `deactivate`
+
+## 2. Structure
+### 2.1 Structure of Auto-downloader
+This is an automated downloader collecting a specific exercise from a specific coursesite on ADAM for all students that handed in something.
+
+##### How To Run The Program
+
+- Download specific exercise from specific course:
+  - Optionally, start virtual environment:
+    - `cd autograder`
+    - `source venv/bin/activate`
+  - `python3 adam.py -u USERNAME -p PASSWORD -n EXERCISENR`
+    - optional args: coursesite, mode, ...; for help and list, see `python3 adam.py -h`
+    - This script logs in and joins the default course page on ADAM and downloads all hand-ins from given exercise as zip file. It then unpacks and moves the hand-ins to the `autograder` directory as `autograder/EXERCISENAME/Abgaben/...`.
+
+- To alter course and exercise infos for the download consult the TODOs in `adam.py`.
+
+### 2.2 Structure of Auto-grader
 
 The packages are:
 
@@ -9,29 +47,21 @@ The packages are:
 - `main.py`
 - `analysis`
 
-#### How To Run The Program
+##### How To Run The Program
 
 - Download all Hand-Ins from Adam and Extract in the **working directory**
-
 - Copy all the necessary tests from the Insurance Week
-
 - Then the execution must the following:
-
   - `main.py` If the programm is done, it returns a 0
-
     - It creates a feedback_YMD folder (Which has the otter grader output as feedback)
-
     - It has the raw grades:
-
       If all tests were correct: 1 1 1 for Question 1, Question 2, Question 3
-
       If one test failed eg. Question 1, the grading is: 0 1 1
-
       If all tests failed, then the output is 0 0 0 
-
       The names, e-mail, individual points are stored in: `grades_raw_YMD.csv`
-
   - To calculate the Bonus-Points and make a bar-plot use: `analysis.py`
-
     - This output is in the analysis folder
 
+### 2.3 Structure of Auto-publisher
+
+[WIP]
--- adam.py
+++ adam.py
@@ -9,54 +9,60 @@ from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import Select
 
 
-coursepage = "https://adam.unibas.ch/goto_adam_crs_1688235.html" # TODO: specify ADAM coursesite
-download_path = "/home/josph/Downloads/" # TODO: adjust downloadpath of selenium firefox browser
+COURSEPAGE = "https://adam.unibas.ch/goto_adam_crs_1688235.html" # TODO: specify ADAM coursesite
+COURSE = "'Intro to Data Science'" # TODO: specify course name
+SUBMISSION_TYPE = "Exercises" # TODO: specify exact submission type link name, e.g. "Exercises" or "Exam insurance"
+HANDIN_PREFIX = "Exercise sheet " # TODO: spcify exact hand-in name link prefix (name of the hand-in without iterator, e.g. "Exercise sheet " or "Insurance ", be mindful of the extra space at the end)
+
+DOWNLOAD_PATH = "/home/josph/Downloads/" # TODO: adjust downloadpath of selenium firefox browser
 
 
 def main(argv):
 
     # parse command line arguments
     try:
-        username, pwd, mode, nr, url = parse_args()
-        print(f"Username: {username}, Password: {pwd}, Mode: {mode}, Number: {nr}, Course URL: {url}")
+        USERNAME, PWD, MODE, NR, URL = parse_args()
+        print(f"Username: {USERNAME}, Password: HIDDEN, Mode: {MODE}, Number: {NR}, Course URL: {URL}")
     except Exception as e:
         print(f"Error parsing arguments: {e}")
         sys.exit(1)
 
-    url = coursepage
-    target_id = url.replace("https://adam.unibas.ch/goto_adam_", "").replace(".html", "")
+    
+    SUBMISSION_NAME = HANDIN_PREFIX + str(NR) 
 
+    target_id = URL.replace("https://adam.unibas.ch/goto_adam_", "").replace(".html", "")
+
+    print("Starting auto-download from browser...")
     # automate ADAM access
     driver = webdriver.Firefox()
 
-    success = login(driver, username, pwd, target_id)
+    # login procedure
+    success = login(driver, USERNAME, PWD, target_id)
     if not success:
         driver.close()
         print("Login was not successful...")
         return
-    
     success = wait_for(driver, "il_mhead_t_focus")
     if not success:
         return False
-    # click exercises link
-    exercise_link = driver.find_element(By.LINK_TEXT, "Exercises")
+    
+    # click exercises link on the ADAM course page
+    exercise_link = driver.find_element(By.LINK_TEXT, SUBMISSION_TYPE)
     exercise_link.click()
 
     success = wait_for(driver, "il_mhead_t_focus")
     if not success:
         return False
 
+    # "Abgaben und Noten" Button
     exercise_link = driver.find_element(By.LINK_TEXT, "Abgaben und Noten")
     exercise_link.click()
 
-    if nr > 1:
-        # Find the select element by its name or ID
+    # select Exercise sheet in dropdown menu based on <nr> argument
+    if NR > 1:
         select_element = driver.find_element(By.ID, "ass_id")
-        # Create a Select object
         select_object = Select(select_element)
-        # Select the option by visible text
-        select_object.select_by_visible_text("Exercise sheet " + str(nr))
-        # Find the button by its name attribute and click on it
+        select_object.select_by_visible_text(SUBMISSION_NAME)
         button = driver.find_element(By.NAME, "cmd[selectAssignment]")
         button.click()
 
@@ -64,50 +70,52 @@ def main(argv):
         if not success:
             return False
     
+    # dowload all submissions of chosen exercise
     button = driver.find_element(By.NAME, "cmd[downloadSubmissions]")
     button.click()
     success = wait_for(driver, "il_mhead_t_focus")
     if not success:
         return False
     
-    # Background tasks bell
-    bell_icon = driver.find_element(By.CSS_SELECTOR, ".glyphicon.glyphicon-bell")
+    # "Background tasks" bell
+    # bell_icon = driver.find_element(By.CSS_SELECTOR, ".glyphicon.glyphicon-bell")
+    bell_icon = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".glyphicon.glyphicon-bell")))
     bell_icon.click()
 
     # "Background tasks" button
-    link = driver.find_element(By.LINK_TEXT, "Background Tasks")
+    link = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.LINK_TEXT, "Background Tasks")))
     link.click()
 
     # "Exercise sheet <nr>" download button
-    elem = "//button[contains(text(), 'Exercise sheet " + str(nr) + "')]"
-    button = driver.find_element(By.XPATH, elem)
+    elem = f"//button[contains(text(), '{SUBMISSION_NAME}')]"
+    button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, elem)))
     button.click()
 
-    # move downloaded zip file into current directory
-    source_path = download_path + 'Exercise sheet ' + str(nr) + '.zip'
+    # move downloaded ZIP file into current directory
+    source_path = DOWNLOAD_PATH + SUBMISSION_NAME + '.zip'
     destination_directory = os.getcwd()  # Get the current working directory
-    destination_path = os.path.join(destination_directory, 'Exercise sheet ' + str(nr) + '.zip')
+    destination_path = os.path.join(destination_directory, SUBMISSION_NAME + '.zip')
     shutil.move(source_path, destination_path)
     
     # unpack zip in current directory and delete zip
     # Specify the name of the ZIP file
-    zip_file_name = 'Exercise sheet ' + str(nr) + '.zip'
-    print(f"Extracted all contents of {zip_file_name} to the current directory.")
+    zip_file_name = SUBMISSION_NAME + '.zip'
 
-    # Ensure the ZIP file exists
+    # ensure the ZIP file exists, open and extract
     if os.path.exists(zip_file_name):
-        # Open the ZIP file in read mode
         with zipfile.ZipFile(zip_file_name, 'r') as zip_ref:
-            # Extract all the contents into the current directory
             zip_ref.extractall('.')
-            print(f"Extracted all contents of {zip_file_name} to the current directory.")
+            print(f"Extracted all contents of '{zip_file_name}' to the current directory.")
         
-        # Remove the ZIP file after extraction
+        # remove the ZIP file after extraction
         os.remove(zip_file_name)
-        print(f"Deleted the ZIP file: {zip_file_name}.")
+        print(f"Deleted the ZIP file: '{zip_file_name}'")
     else:
-        print(f"The file {zip_file_name} does not exist.")
+        print(f"The file '{zip_file_name}' does not exist.")
 
+    print(f"\n==========================================================================")
+    print(f"'{SUBMISSION_NAME}' from the course {COURSE} and hand-in type '{SUBMISSION_TYPE}' \nhas been downloaded into the current directory.")
+    print(f"==========================================================================\n")
 
     # close browser
     driver.close()
@@ -126,7 +134,7 @@ def parse_args():
     parser.add_argument('-p', '--password', type=str, required=True, help='Password')
     parser.add_argument('-n', '--nr', type=int, required=True, help='Number')
     parser.add_argument('-m', '--mode', type=str, required=False, default='default_mode', help='Mode (optional)')
-    parser.add_argument('-c', '--coursesite', type=str, required=False, default=coursepage, help='Course URL (optional)')
+    parser.add_argument('-c', '--coursesite', type=str, required=False, default=COURSEPAGE, help='Course URL (optional)')
 
     args = parser.parse_args()
 
